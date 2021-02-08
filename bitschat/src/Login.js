c13d5b0af25ff3c3508d50724880ef6f79007172
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-02-08 10:03:56 +0530

    Login

---
+++ bitschat/src/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Login.css';
+import {Button} from '@material-ui/core';
+
+function Login() {
+
+    const signIn = () => {}
+    return (
+        <div className='login'>
+            
+            <div className='login_container'>
+                <div className = 'login_text'>
+                    <h1> Sign in to BITSChat</h1>
+                </div>
+                <Button onClick={signIn}>
+                    Sign In with Google
+                </Button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Login
