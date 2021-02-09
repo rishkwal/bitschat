9fbc1d8e631882bcda60d8ee34b60b3f76a3e0cf
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-02-09 18:53:11 +0530

    Google Authentication added

--- bitschat/src/App.js
+++ bitschat/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
-import { useState} from 'react';
+import { useStateValue} from './StateProvider';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './Login'
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [{user}, dispatch] = useStateValue();
   return (
     
     <div className="app">
--- bitschat/src/Login.js
+++ bitschat/src/Login.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './Login.css';
 import {Button} from '@material-ui/core';
+import {auth, provider} from './firebase';
+import { actionTypes } from './reducer';
+import { useStateValue} from './StateProvider';
+
 
 
 function Login() {
+    const[, dispatch] = useStateValue();
 
-    const signIn = () => {}
+    const signIn = () => {
+        auth
+        .signInWithPopup(provider)
+        .then((result) => {
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.user,
+            })
+        })
+        .catch((error) => alert(error.message));
+
+    };
     return (
         <div className='login'>
             
