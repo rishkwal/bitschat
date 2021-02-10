f06e0e0fc8633dde637b6762a463858b3c04fbb1
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-02-10 12:25:25 +0530

    Google Avatar

--- bitschat/src/Sidebar.js
+++ bitschat/src/Sidebar.js
@@ -7,8 +7,11 @@ import SearchIcon from '@material-ui/icons/Search';
 import SidebarChat from './SidebarChat'
 import {useState, useEffect} from 'react';
 import db from './firebase';
+import { useStateValue } from './StateProvider';
+
 function Sidebar(){
     const[rooms, setRooms] = useState([]);
+    const[{ user}, dispatch] = useStateValue();
 
     useEffect(()=> {
         const unsubscribe = db.collection('rooms').onSnapshot(snapshot=> (
@@ -26,7 +29,7 @@ function Sidebar(){
 return( 
         <div className="sidebar"> 
             <div className = 'sidebar_header'>
-            <Avatar/>
+            <Avatar src = {user?.photoURL}/>
             <div className='sidebar_headerRight'>
                 <IconButton>
                     <DonutLargeIcon/>
