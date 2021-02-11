d0fe59101063fb38927339633f73ad4843368286
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-02-11 11:46:10 +0530

    last message feature added

--- bitschat/src/SidebarChat.js
+++ bitschat/src/SidebarChat.js
@@ -2,9 +2,26 @@ import './SidebarChat.css';
 import {Avatar} from '@material-ui/core';
 import db from './firebase';
 import { Link } from "react-router-dom";
+import {useState, useEffect} from 'react';
 
 function SidebarChat({id, name, addNewChat}) {
     var avatarLink = `https://ui-avatars.com/api/?name=${name}`;
+const [messages, setMessages] = useState("");
+
+useEffect(()=> {
+    if(id) {
+        db.collection('rooms')
+        .doc(id)
+        .collection('messages')
+        .orderBy('timestamp','desc')
+        .onSnapshot((snapshot) => 
+            setMessages(snapshot.docs.map((doc)=>
+            doc.data()))
+        )
+        
+    }
+},[]);
+
     const createChat =() => {
         const roomName = prompt("Please enter name for chat room")
     if (roomName) {
@@ -20,7 +37,7 @@ return !addNewChat ? (
     <Avatar src ={avatarLink}/>
     <div className = 'SidebarChat_info'>
         <h2>{name}</h2>
-        <p>Last message...</p> 
+        <p>{messages[0]?.message}</p> 
     </div>
 </div>
     </Link>
